ca672260c9cdba2d14d36c2e0a98e0b30a454271
leeyjwinter <james_22@naver.com>
2021-11-23 16:28:45 +0900

    commit

---
+++ javascript/app.js
@@ -0,0 +1,10 @@
+console.log(454545);//콘솔로 데이터 전송
+console.log("hello");
+
+const a = 5;
+const b = 2;
+const myName = "lee";
+console.log(a + b);
+console.log(a * b);
+console.log(a / b);
+console.log("hello" + " " + myName);
