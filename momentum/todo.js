e80885f1645d69803d1b7fe66fc8eb14ad119bc5
Lee Young Jun <leeyoung123321@gmail.com>
2022-02-03 17:45:30 +0900

    commit

--- momentum/todo.js
+++ momentum/todo.js
@@ -2,18 +2,39 @@ const toDoForm = document.getElementById("todo-form");
 const toDoList = document.getElementById("todo-list");
 const toDoInput = toDoForm.querySelector("input");
 
-function paintToDo(){
+function deleteTodo(event){
+    clickedList = event.target.parentElement.innerText;
+    const li = event.target.parentElement;
+    //console.log(clickedList);
+    li.remove();
+    
+    
+}
+
+function paintToDo(myTodo){
+
+    //li를 만들고 그 안에 span넣고 싶음
+    const li = document.createElement("li");
+    const span = document.createElement("span");
+    span.innerText = myTodo;
+    const button = document.createElement("button");
+    button.innerText = "❌";
+    button.addEventListener("click", deleteTodo);
+    li.appendChild(span);
+    li.appendChild(button);
+    toDoList.appendChild(li);
 
 }
 
 
 function onTodoSubmitEvent(event){
     event.preventDefault();
-    mytodo = toDoInput.value;
+    myTodo = toDoInput.value;
     // 폼 안의 내용 비워줌
     toDoInput.value = ""
-    // localStorage.setItem("mytodo",mytodo);
-    // newTodo =  localStorage.getItem("mytodo");
+    paintToDo(myTodo);
+    localStorage.setItem("myTodo",myTodo);
+    // newTodo =  localStorage.getItem("myTodo");
     // toDoList.innerText = newTodo;
 }
 
